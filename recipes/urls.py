cb000a2696aa748701594d875729e57db4c509ef
Guilherme K Pinheiro <guilherme_gkp@hotmail.com>
2022-04-10 21:17:01 -0300

    home head

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 
-from recipes.views import contato, home, sobre
+from recipes.views import home
 
 # HTTP request -> <- HTTP respons
 
 urlpatterns = [
     path('', home),  # Home
-    path('sobre/', sobre),  # /sobre/
-    path('contato/', contato),  # /contato/
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,18 +1,9 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'recipes/home.html', context={
+    return render(request, 'recipes/pages/home.html', context={
         'name': 'Guilherme Pinheiro'
     })
-
-
-def sobre(request):
-    return render(request, 'me-apague/temp.html')
-
-
-def contato(request):
-    return HttpResponse('999999999')
